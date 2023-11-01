d680a149f0bd5d783442a46c65f5bd9c92d85d10
Renato Figueiredo <rjofig@gmail.com>
2023-11-01 18:40:54 -0400

    Update rocker_setup.R
    replaced 's

--- rocker_setup.R
+++ rocker_setup.R
@@ -1,18 +1,18 @@
 # install dependences: curl, git, gh
-system(‘sudo apt-get update’)
-system(‘sudo apt-get install git-all -y’)
-system(‘curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null && sudo apt update && sudo apt install gh -y’)
+system('sudo apt-get update')
+system('sudo apt-get install git-all -y')
+system('curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null && sudo apt update && sudo apt install gh -y')
 
 # install dependences - devtools
-install.packages(‘devtools’)
-library(‘devtools’)
+install.packages('devtools')
+library('devtools')
 
 # install dependences - minioclient
-install.packages(‘minioclient’)
-library(‘minioclient’)
+install.packages('minioclient')
+library('minioclient')
 install_mc()
-mc_mb(‘play/faasr’)
+mc_mb('play/faasr')
 
 # install FaaSr
 devtools::install_github('FaaSr/FaaSr-package',ref='main',force=TRUE)
-library(‘FaaSr’)
+library('FaaSr')
