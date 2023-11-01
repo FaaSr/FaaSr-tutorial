035b6eae590cad9fd327a309c230e3758d47cc5c
Renato Figueiredo <rjofig@gmail.com>
2023-11-01 18:45:13 -0400

    Update rocker_setup.R
    added curl

--- rocker_setup.R
+++ rocker_setup.R
@@ -1,5 +1,6 @@
 # install dependences: curl, git, gh
 system('sudo apt-get update')
+system('sudo apt-get install curl -y')
 system('sudo apt-get install git-all -y')
 system('curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null && sudo apt update && sudo apt install gh -y')
 
